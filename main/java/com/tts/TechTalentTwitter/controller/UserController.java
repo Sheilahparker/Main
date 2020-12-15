1be00741fb3443087f6166823a17fc19d5e77974
Sheilah <73185099+Sheilahparker@users.noreply.github.com>
2020-12-15 13:23:47 -0500

    Add files via upload

---
+++ main/java/com/tts/TechTalentTwitter/controller/UserController.java
@@ -0,0 +1,109 @@
+package com.tts.TechTalentTwitter.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.tts.TechTalentTwitter.model.Tweet;
+import com.tts.TechTalentTwitter.model.User;
+import com.tts.TechTalentTwitter.service.TweetService;
+import com.tts.TechTalentTwitter.service.UserService;
+import com.tts.TechTalentTwitter.model.TweetDisplay;
+import com.tts.TechTalentTwitter.repository.TweetRepository;
+
+@Controller
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private TweetService tweetService;
+
+	@GetMapping(value = "/profile")
+	public String getUserLogged(Model model) {
+		User loggedInUser = userService.getLoggedInUser();
+		return "redirect:users/" + loggedInUser.getUsername();
+	}
+
+	@GetMapping(value = "/users/{username}")
+	public String getUser(@PathVariable(value = "username") String username, Model model) {
+		User loggedInUser = userService.getLoggedInUser();
+		User user = userService.findByUsername(username);
+		List<TweetDisplay> tweets = tweetService.findAllByUser(user);
+		List<User> following = loggedInUser.getFollowing();
+		boolean isFollowing = false;
+		for (User followedUser : following) {
+			if (followedUser.getUsername().equals(username)) {
+				isFollowing = true;
+			}
+		}
+		boolean isSelfPage = loggedInUser.getUsername().equals(username);
+		model.addAttribute("tweetList", tweets);
+		model.addAttribute("user", user);
+		model.addAttribute("following", isFollowing);
+		model.addAttribute("isSelfPage", isSelfPage);
+		return "user";
+	}
+
+	@GetMapping(value = "/users")
+	public String getUsers(@RequestParam(value = "filter", required = false) String filter, Model model) {
+		List<User> users = new ArrayList<User>();
+
+		User loggedInUser = userService.getLoggedInUser();
+
+		// List of Followers 
+		List<User> usersFollowing = loggedInUser.getFollowing();
+		List<User> usersFollowers = loggedInUser.getFollowers();
+		if (filter == null) {
+			filter = "all";
+		}
+		if (filter.equalsIgnoreCase("followers")) {  // getting followers 
+			users = usersFollowers;
+			model.addAttribute("filter", "followers");
+			
+		} else if (filter.equalsIgnoreCase("following")) { // getting user's followers
+			users = usersFollowing;
+			model.addAttribute("filter", "following");
+		} else {
+			users = userService.findAll();
+			model.addAttribute("filter", "all"); // getting all users
+		}
+		model.addAttribute("users", users);
+
+		SetTweetCounts(users, model);
+		SetFollowingStatus(users, usersFollowing, model);
+
+		return "users";
+	}
+
+	private void SetTweetCounts(List<User> users, Model model) {
+		HashMap<String, Integer> tweetCounts = new HashMap<>();
+		for (User user : users) {
+			List<TweetDisplay> tweets = tweetService.findAllByUser(user);
+			tweetCounts.put(user.getUsername(), tweets.size());
+		}
+		model.addAttribute("tweetCounts", tweetCounts);
+	}
+
+	private void SetFollowingStatus(List<User> users, List<User> usersFollowing, Model model) {
+		HashMap<String, Boolean> followingStatus = new HashMap<>();
+		String username = userService.getLoggedInUser().getUsername();
+		for (User user : users) {
+			if (usersFollowing.contains(user)) {
+				followingStatus.put(user.getUsername(), true);
+			} else if (!user.getUsername().equals(username)) {
+				followingStatus.put(user.getUsername(), false);
+			}
+		}
+		model.addAttribute("followingStatus", followingStatus);
+	}
+}
